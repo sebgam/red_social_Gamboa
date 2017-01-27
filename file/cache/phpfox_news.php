da579f4732ff465f81c52d63f2f1b7e717011b19
sebas <sebastiangamboa11@gmail.com>
2017-01-27 17:20:41 -0500

    config

---
+++ file/cache/phpfox_news.php
@@ -0,0 +1,38 @@
+<?php defined('PHPFOX') or exit('NO DICE!'); ?>
+<?php $aContent = array (
+  0 => 
+  array (
+    'title' => 'phpFox 4.5.1 Build 2 has been released',
+    'link' => 'http://feedproxy.google.com/~r/phpfox/~3/TSekMu5g7lc/',
+    'creator' => 'phpFox Admin',
+    'time_stamp' => 1485338506,
+  ),
+  1 => 
+  array (
+    'title' => 'phpFox 2017 Plan',
+    'link' => 'http://feedproxy.google.com/~r/phpfox/~3/7SxNXs9u6ko/',
+    'creator' => 'phpFox Admin',
+    'time_stamp' => 1485279049,
+  ),
+  2 => 
+  array (
+    'title' => 'phpFox 4.5.1 is ready now!',
+    'link' => 'http://feedproxy.google.com/~r/phpfox/~3/O4EABDD_xdc/',
+    'creator' => 'phpFox Admin',
+    'time_stamp' => 1484824504,
+  ),
+  3 => 
+  array (
+    'title' => 'phpFox 2016 In Review',
+    'link' => 'http://feedproxy.google.com/~r/phpfox/~3/utXQ0Y_vAbw/',
+    'creator' => 'phpFox Admin',
+    'time_stamp' => 1483098966,
+  ),
+  4 => 
+  array (
+    'title' => 'phpFox 4.5.0 Build 3 has been released ? PHPMailer updated',
+    'link' => 'http://feedproxy.google.com/~r/phpfox/~3/DIRrrR-8ylQ/',
+    'creator' => 'phpFox Admin',
+    'time_stamp' => 1482918436,
+  ),
+); ?>
